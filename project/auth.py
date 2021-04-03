4a0e5d440ad011defefc6e3777e949bd39fe233f
sonify <givemeparachute@gmail.com>
2021-04-03 15:29:41 +0300

    попытка включить логирование

--- project/auth.py
+++ project/auth.py
@@ -161,7 +161,8 @@ def signup_post():
     html = render_template('activate.html', confirm_url=confirm_url)
     subject = "Please confirm your email"
     try:
-        send_email(new_user.email, subject, html)
+        maildebug = send_email(new_user.email, subject, html)
+        print(maildebug)
         flash('A confirmation email has been sent via email.', 'success')
     except SMTPException:
         flash('We have some problems with sending emails. Please try again later :(')
